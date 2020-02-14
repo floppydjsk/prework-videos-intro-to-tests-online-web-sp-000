93ab57133c0c3b1ea5be6a547a10122e2fdf4d9f
Daniel Kim <daniel.kim52@gmail.com>
2020-02-14 09:07:12 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -4,7 +4,7 @@ describe "conversions" do
   describe '#ounces_to_grams' do
     it 'given zero, returns 0.0' do
       grams = ounces_to_grams(2)
-      expect(grams).to eq(0.0)
+      expect(grams).to eq(2.0)
     end
   end
 end
